0a8038dd0bbef7ef4963dd999ddfcca81a06662b
bala <bala.springboot@gmail.com>
2022-11-15 11:00:43 +0530

    Initial version

---
+++ src/main/java/com/bala/jwt/model/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.bala.jwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+
+    private String jwtToken;
+
+}
